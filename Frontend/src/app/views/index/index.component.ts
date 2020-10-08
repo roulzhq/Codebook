d7cffeb1d52d08a85fc7e0982ea6b928b1d6144a
Janis Jansen <janis.jansen@gmx.de>
2020-10-08 14:35:59 +0200

    Cleaned up markup/style

--- Frontend/src/app/views/index/index.component.ts
+++ Frontend/src/app/views/index/index.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { faArrowAltCircleDown } from '@fortawesome/free-regular-svg-icons';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'index-view',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexView implements OnInit {
+  constructor() {}
 
-  public faArrowAltCircleDown= faArrowAltCircleDown;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
