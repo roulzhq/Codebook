3fbdb3298be0ce97a3f291cf280f7d925ca155fd
Janis Jansen <janis.jansen@gmx.de>
2020-10-09 19:28:34 +0200

    Added firebase-based auth guards and redirects

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,17 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  AngularFireAuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+
 // Views
 import { IndexView } from './views/index/index.component';
 import { CodebookListView } from './views/codebook-list/codebook-list.component';
 import { CodebookView } from './views/codebook/codebook.component';
 import { AuthView } from './views/auth/auth.component';
 
-const routes: Routes = [
-  { path: '', component: IndexView, pathMatch: 'full' },
-  { path: 'codebook/:id', component: CodebookView },
-  { path: 'codebooks', component: CodebookListView },
+const redirectUnauthorizedToAuth = () => redirectUnauthorizedTo(['auth']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
 
-  { path: 'auth', component: AuthView },
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexView,
+    pathMatch: 'full',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToAuth },
+  },
+
+  {
+    path: 'codebook/:id',
+    component: CodebookView,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToAuth },
+  },
+  {
+    path: 'codebooks',
+    component: CodebookListView,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToAuth },
+  },
+
+  {
+    path: 'auth',
+    component: AuthView,
+    data: { authGuardPipe: redirectLoggedInToHome },
+  },
 ];
 
 @NgModule({
