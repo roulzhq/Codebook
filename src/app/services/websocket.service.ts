76c6fb7c9cf4a3651023d0b715a762d860557629
Janis Jansen <janis.jansen@gmx.de>
2020-12-20 14:20:23 +0100

    Update websocket.service.ts

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,28 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Observable, Observer, Subject } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebsocketService {
-  constructor() {
-    let socket = new WebSocket(`ws://localhost:8080/clientHandler`);
-
-    socket.onopen = (event) => {
-      socket.send(
-        JSON.stringify({
-          messageID: "12345-12345",
-          messageType: 1,
-          authToken: '129031-d12-112d08dh10fg4-132d13',
-          data: {
-            codebookId: 'mycodebook',
-            cellId: 'acellid',
-          },
-        })
-      );
-    };
-
-    socket.onclose = (event) => {
-      console.log(event);
-    };
+  public connect(url: string): WebSocketSubject<MessageEvent> {
+    return webSocket(url);
   }
 }
